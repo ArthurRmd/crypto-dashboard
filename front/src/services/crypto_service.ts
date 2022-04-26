855382ce3b7f17d2bb9be500e6cf3452a33d1fa2
Rolf1e <tigran.slama@laposte.net>
2022-04-26 14:30:12 +0200

    Add some fake data for render

--- front/src/services/crypto_service.ts
+++ front/src/services/crypto_service.ts
@@ -11,16 +11,24 @@ export class CryptoService {
   private constructor() { }
 
   public async fetchNewChanges(): Promise<DashBoardCryptDo[]> {
-    return await axios.get<DashBoardCryptDo[]>(FETCH_NEW_CHANGES)
-      .then((response) => {
-        const status = response.status;
-        if (status === 200) {
-          return response.data;
-        }
+    return Promise.resolve(this.fakeData());
+    // return await axios.get<DashBoardCryptDo[]>(FETCH_NEW_CHANGES)
+      // .then((response) => {
+        // const status = response.status;
+        // if (status === 200) {
+          // return response.data;
+        // }
+// 
+        // throw new Error('Failed to fetch new changes for dashboard on ' + FETCH_NEW_CHANGES + ' with status ' + status);
+      // }).catch((error) => {
+        // throw new Error('Failed to fetch new changes for dashboard on ' + FETCH_NEW_CHANGES, error);
+      // });
+  }
 
-        throw new Error('Failed to fetch new changes for dashboard on ' + FETCH_NEW_CHANGES + ' with status ' + status);
-      }).catch((error) => {
-        throw new Error('Failed to fetch new changes for dashboard on ' + FETCH_NEW_CHANGES, error);
-      });
+  private fakeData(): DashBoardCryptDo[] {
+    return [
+      DashBoardCryptDo.create('BTC', 1.05, 870.17, 256.98),
+      DashBoardCryptDo.create('ETH', 5.35, 1050.45, 190.11),
+    ];
   }
 }
