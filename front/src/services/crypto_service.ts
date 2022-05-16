fdd0afc91998a20a16f7846dff46a2cab5dcdd1f
Rolf1e <tigran.slama@laposte.net>
2022-05-16 20:26:44 +0200

    [Front] crypt service is enable

--- front/src/services/crypto_service.ts
+++ front/src/services/crypto_service.ts
@@ -12,24 +12,17 @@ export class CryptoService {
     }
 
     public async fetchNewChanges(): Promise<DashBoardCryptDo[]> {
-        return Promise.resolve(CryptoService.fakeData());
-        // return await axios.get<DashBoardCryptDo[]>(FETCH_NEW_CHANGES)
-        // .then((response) => {
-        // const status = response.status;
-        // if (status === 200) {
-        // return response.data;
-        // }
-// 
-        // throw new Error('Failed to fetch new changes for dashboard on ' + FETCH_NEW_CHANGES + ' with status ' + status);
-        // }).catch((error) => {
-        // throw new Error('Failed to fetch new changes for dashboard on ' + FETCH_NEW_CHANGES, error);
-        // });
+        return await axios.get<DashBoardCryptDo[]>(SERVER_FETCH_NEW_CHANGES)
+            .then((response) => {
+                const status = response.status;
+                if (status === 200) {
+                    return response.data;
+                }
+
+                throw new Error('Failed to fetch new changes for dashboard on ' + SERVER_FETCH_NEW_CHANGES + ' with status ' + status);
+            }).catch((error) => {
+                throw new Error('Failed to fetch new changes for dashboard on ' + SERVER_FETCH_NEW_CHANGES, error);
+            });
     }
 
-    private static fakeData(): DashBoardCryptDo[] {
-        return [
-            {currencyName: 'BTC', gainLast24Hours: 1.05, totalWallet: 870.17, totalWalletDifference: 256.98},
-            {currencyName: 'ETH', gainLast24Hours: 5.35, totalWallet: 1050.45, totalWalletDifference: 190.11},
-        ];
-    }
 }
